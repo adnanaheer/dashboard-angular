643937b0ee7edd366ae2aded5614aafbaea0dfcd
adnanaheer <smart7897>
2020-11-25 12:55:32 +0500

    fixed console errors

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { FormGroup, } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -9,34 +6,9 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
-
   hide = true;
 
-  email = new FormControl('', [Validators.required]);
-
-  LoginFormComponent: FormGroup;
-
-  constructor(private fb: FormBuilder) { }
-
   ngOnInit(): void {
-    this.LoginFormComponent = this.fb.group({
-      email: ['', [Validators.required, Validators.pattern('[a-z0-9.@]*')]],
-    });
-  }
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-
-      return 'Enter your Value';
-
-    }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  onSubmit(form: FormGroup) {
-    console.log('Valid?', form.valid); // true or false
-    console.log('Email', form.value.email);
-
   }
 
 }
